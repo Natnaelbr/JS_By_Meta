737f4198cb155c04db9090740cd2728fe06bbecc
Natnaelbr <natnaelbirhan1440@gmail.com>
2024-01-24 09:59:05 +0300

    Coditional statements

---
+++ conditionals.js
@@ -0,0 +1,19 @@
+//Conditional statement "IF"
+var result = 41;
+if (result > 40) {
+    console.log("Cogradulation you passed.");
+} else {
+    console.log("Unfortunately you did not pass.")
+}
+
+//Coditional statement "IF ELSE"
+var food = "cold";
+if (food == "hot") {
+    console.log('Too hot')
+}
+else if (food == "cold") {
+    console.log('Too cold')
+}
+else {
+    console.log('Just right')
+}
