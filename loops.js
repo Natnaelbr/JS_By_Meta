ef751dce70e30df442b1a5397116f2634e48a691
Natnaelbr <natnaelbirhan1440@gmail.com>
2024-03-14 15:17:02 +0300

    For While Nested Loops

--- loops.js
+++ loops.js
@@ -54,3 +54,11 @@ for (var i = 100; i > 10; i = i - 10) {
         console.log(i + " divided by " + j + " equals " + i / j);
     }
 }
+
+//Simple Example Uses of Loops
+var cubes = 'ABCDEFG';
+//styling console output using CSS with a %c format specifier
+for (var i = 0; i < 7; i++) {
+    var styles = "font-size: 40px; border-radius: 10px; border: 1px solid blue; background: pink; color: purple";
+    console.log("%c" + cubes[i], styles)
+}
